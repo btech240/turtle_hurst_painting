a58b5f5664e557f9c462ab833112675e4eca924c
btech240 <brenttlong@gmail.com>
2024-01-07 15:06:37 -0500

    Used colorgram to get colors from host image, commented out that code

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 import random
 import turtle as t
 from turtle import Screen
+
+# import colorgram
+
+# # Extract Colors from image using colorgram library, then saved and added to color_list
+# colors = colorgram.extract('./image.jpg', 8)
+
+# # Create list of colors
+# rgb_colors = []
+# for color in colors:
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
+#     new_color = (r, g, b)
+#     rgb_colors.append(new_color)
+
+color_list = [(232, 232, 232), (215, 158, 75), (189, 176, 33), (235, 70, 31), (181, 45, 67), (217, 227, 220), (156, 31, 44),
+              (68, 35, 56), (121, 164, 196), (232, 209, 104), (193, 71, 40), (215, 63, 110), (56, 49, 107), (218, 129, 164)]
